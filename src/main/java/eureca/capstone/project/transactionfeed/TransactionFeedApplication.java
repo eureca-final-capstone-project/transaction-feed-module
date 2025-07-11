9af4370b770df432c8815cf46a2254c2c10d3bf6
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-11 09:02:15 +0900

    first commit

---
+++ src/main/java/eureca/capstone/project/transactionfeed/TransactionFeedApplication.java
@@ -0,0 +1,13 @@
+package eureca.capstone.project.transactionfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionFeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eureca/capstone/project/transactionfeed/TransactionFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package eureca.capstone.project.transactionfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionFeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
